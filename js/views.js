84036423c39ea40982d739f2731a6aa653858c3b
Daniel Copeland <copelandic24@gmail.com>
2013-03-22 12:56:48 +0000

    instance of header moved into render

--- js/views.js
+++ js/views.js
@@ -3,11 +3,11 @@ App.Views.Teams = Backbone.View.extend({
 	header: template('headerTemplate'),
 
 	initialize : function() {
-		this.listenTo( this.collection, 'reset', this.render );
-		this.$el.html( this.header() );
+		this.listenTo( this.collection, 'sort', this.render );
 	},
 
 	render: function() {
+		this.$el.html( this.header() );
 		this.collection.each(function(team) {
 			var teamView = new App.Views.Team({ model: team });
 			this.$el.append(teamView.render().el);
@@ -21,7 +21,7 @@ App.Views.Teams = Backbone.View.extend({
 
 	sortedBy : function(event) {
 		var type = $(event.target).attr('data-type');
-		this.collection.sortBy( type );
+		this.collection.sortedBy( type );
 	}
 });
 
